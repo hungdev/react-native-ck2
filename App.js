00d8a2fbb4ac941ab7261546f172a583996d79d8
cee <hungns126@gmail.com>
2021-04-06 20:51:08 +0700

    add product screen

--- App.js
+++ App.js
@@ -1,112 +1,71 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React from 'react'
+import { View, Text, TouchableOpacity, FlatList, Image, StyleSheet } from 'react-native'
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+// const DATA = [
+//   {
+//     id: 1,
+//     photo: 'https://anhdepblog.com/wp-content/uploads/2020/09/anh-gai-xinh-facebook-21.jpg',
+//     name: 'ao 2 day',
+//     price: '2000$',
+//     star: 4
+//   }
+// ]
+const DATA = Array(10).fill('').map((e, i) => ({
+  id: i + 1,
+  photo: 'https://anhdepblog.com/wp-content/uploads/2020/09/anh-gai-xinh-facebook-21.jpg',
+  name: `ao 2 day ${i + 1}`,
+  price: '2000$',
+  heart: i % 2 === 0
+}))
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+export default function App() {
+  const renderItem = ({ item }) => (
+    <View style={{ width: '45%', }}>
+      <Image
+        style={styles.imgStyle}
+        source={{ uri: item.photo, }}
+      />
+      <View style={styles.rowPrice}>
+        <Text>{item.price}</Text>
+        <Ionicons name="heart" size={30} color={item.heart ? 'red' : 'grey'} />
+      </View>
+      <Text>{item.name}</Text>
     </View>
   );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
+    <View>
+      <View style={{ flexDirection: 'row', borderBottomWidth: 1, borderBottomColor: 'grey' }}>
+        <TouchableOpacity style={{ flexDirection: 'row', flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <Text>SORT</Text>
+          <Ionicons name="chevron-down-outline" size={30} color="black" />
+        </TouchableOpacity>
+        <TouchableOpacity style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <Text style={{ textAlign: 'center', }}>REFINE</Text>
+        </TouchableOpacity>
+      </View>
+      <Text style={{ textAlign: 'center', marginTop: 15, marginBottom: 20 }}>405 styles</Text>
+      <FlatList
+        data={DATA}
+        numColumns={2}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+        columnWrapperStyle={{ justifyContent: 'space-around', marginBottom: 10, flex: 1 }}
+        style={{ marginBottom: 100 }}
+      />
+    </View>
+  )
+}
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  rowPrice: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 10
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+  imgStyle: {
+    height: 300,
+    width: 'auto'
+  }
 });
-
-export default App;
